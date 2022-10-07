f1f5ca26d45dd631e97dba7c5fe235d96f959c83
SilindaM <silindanemdu@gmail.com>
2022-10-07 15:05:45 +0200

    add login for

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJy_p-nLrJNh4GHHeeSqV3V8CnBwadN-A",
+  authDomain: "netflixclone-f4555.firebaseapp.com",
+  projectId: "netflixclone-f4555",
+  storageBucket: "netflixclone-f4555.appspot.com",
+  messagingSenderId: "10765317592",
+  appId: "1:10765317592:web:9e3d8f91a1250c8d496ead"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
